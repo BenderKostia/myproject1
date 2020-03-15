76f14d0dc782ee377fe53ff7fa421683516ee058
Bender <bondarenko.konstantin.privat@gmail.com>
2020-03-15 17:42:37 +0200

    few changes

--- Hello.py
+++ Hello.py
@@ -1,8 +1,9 @@
 import random
 # test
+# Create list
 alphabet = ['A','B','C','D','E','F','G','H','I','J']
 numbers = [1,2,3,4,5,6,7,8,9,10]
-
+# random choice
 letter = random.choice(alphabet)
 number = random.choice(numbers)
 
